54be6d0061b0c0e341f9f258c292dbeab5c7ffa8
rwtneeraj <183721769+rwtneeraj@users.noreply.github.com>
2024-12-18 15:47:38 +0530

    add sub and mutliplication function

--- demo.js
+++ demo.js
@@ -1,3 +1,12 @@
 const add = function (num1, num2) {
   return num1 + num2;
 };
+
+const sub = function (num1, num2) {
+  return num1 - num2;
+};
+
+const multiplication = function (num1, num2) {
+  return num1 * num2;
+};
+
